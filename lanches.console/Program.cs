b214b40d69b22c43898f5391c49698228ce46541
tiago154 <tiago.toya@hotmail.com>
2018-09-09 17:15:02 -0300

    feat(lanches.domain): Commit inicial

---
+++ lanches.console/Program.cs
@@ -0,0 +1,14 @@
+﻿using lanches.domain.Enums;
+using lanches.domain.ValueObjects;
+using System;
+
+namespace lanches.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
